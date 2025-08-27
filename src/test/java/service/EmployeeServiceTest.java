e94d239d763ef4d0ff52666e9f459b12eac33f62
Joseph Ding <joseph.ding@oocl.com>
2025-08-27 15:32:47 +0800

    test:加入年龄超过65添加员工测试用例

--- src/test/java/service/EmployeeServiceTest.java
+++ src/test/java/service/EmployeeServiceTest.java
@@ -4,14 +4,15 @@ package service;
 import com.oocl.training.Model.Employee;
 import com.oocl.training.service.EmployeeService;
 import com.oocl.training.DAO.EmployeeRepo;
+import exception.OutsideAgeRangeEmployee;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(SpringExtension.class)
@@ -38,5 +39,26 @@ public class EmployeeServiceTest {
         assertEquals(employee.getGender(), createdEmployee.getGender());
         assertEquals(employee.getSalary(), createdEmployee.getSalary());
         assertEquals(employee.getStatus(), createdEmployee.getStatus());
+    }
+
+    @Test
+    void should_throw_exception_when_create_employee_with_age_below_18() {
+        // Given
+        Employee employee = new Employee("John Doe", 17, "MALE", 3000, true);
+
+        //when / then
+        OutsideAgeRangeEmployee exception = assertThrows(OutsideAgeRangeEmployee.class, () -> employeeService.addEmployee(employee));
+        assertEquals("Employee age must be between 18 and 65.", exception.getMessage());
+    }
+
+
+    @Test
+    void should_throw_exception_when_create_employee_with_age_above_65() {
+        // Given
+        Employee employee = new Employee("John Doe", 80, "MALE", 3000, true);
+
+        //when / then
+        OutsideAgeRangeEmployee exception = assertThrows(OutsideAgeRangeEmployee.class, () -> employeeService.addEmployee(employee));
+        assertEquals("Employee age must be between 18 and 65.", exception.getMessage());
     }
 }
